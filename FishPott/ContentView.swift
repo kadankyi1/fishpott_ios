f201876fe259da3ee38f3ded11429bbfae7a44a3
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-19 02:09:11 -0800

    Initial Commit

---
+++ FishPott/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FishPott
+//
+//  Created by Dankyi Anno Kwaku on 11/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FishPott/FishPottApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishPottApp.swift
+//  FishPott
+//
+//  Created by Dankyi Anno Kwaku on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FishPottApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
