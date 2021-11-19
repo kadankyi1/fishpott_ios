1f0ef3f3a0af8cbd991cb6d4787f9fc582a38ff2
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-19 08:16:47 -0800

    Started intro card design

---
+++ FishPott/View/IntroCardView.swift
@@ -0,0 +1,26 @@
+//
+//  IntroCardView.swift
+//  FishPott
+//
+//  Created by Dankyi Anno Kwaku on 11/19/21.
+//
+
+import SwiftUI
+
+struct IntroCardView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: BODY
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+// MARK: - PREVIEW
+struct IntroCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
