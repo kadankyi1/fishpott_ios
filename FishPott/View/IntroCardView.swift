9196d993901ebf06f6132e4c830ba8b9cbf84eae
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-19 08:53:26 -0800

    Set info card 1 background color, image and title

--- FishPott/View/IntroCardView.swift
+++ FishPott/View/IntroCardView.swift
@@ -13,7 +13,28 @@ struct IntroCardView: View {
     
     // MARK: BODY
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack(spacing: 20) {
+                // INFO : IMAGE
+                Image("startinfo1")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                // INFO : TITLE
+                
+                // INFO : HEADLINE
+                
+                // INFO : BUTTON-START
+                Text("Hello, World!")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+            } //  VSTACK
+        } //  ZSTACK
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlackPrimary"), Color("ColorBlackPrimary")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
