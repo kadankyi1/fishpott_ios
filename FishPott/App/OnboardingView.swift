757165a38b871958eb7d6e82d20a5d844b0c4e4b
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-19 09:40:47 -0800

    Added onboarding view to window group

---
+++ FishPott/App/OnboardingView.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingView.swift
+//  FishPott
+//
+//  Created by Dankyi Anno Kwaku on 11/19/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        
+        IntroCardView()
+    }
+}
+
+// MARK - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
