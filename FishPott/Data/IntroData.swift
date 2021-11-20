f05e2866b60f09b1b3923e373b2d87f63473afe3
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-20 01:17:08 -0800

    Completed model for intro data and started data file for intro

---
+++ FishPott/Data/IntroData.swift
@@ -0,0 +1,8 @@
+//
+//  IntroData.swift
+//  FishPott
+//
+//  Created by Dankyi Anno Kwaku on 11/20/21.
+//
+
+import Foundation
---
+++ FishPott/Model/IntroModel.swift
@@ -0,0 +1,21 @@
+//
+//  IntroModel.swift
+//  FishPott
+//
+//  Created by Dankyi Anno Kwaku on 11/20/21.
+//
+
+import SwiftUI
+
+
+// MARK: - INTRO DATA MODEL
+
+struct Intro: Identifiable {
+    var id: UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradidentColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
