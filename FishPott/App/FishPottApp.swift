6193f6f87ef4995d3e2ad39b5ea322fa67b21afd
kadankyi1 <annodankyikwaku@gmail.com>
2022-02-15 22:51:07 -0800

    Resolved fcm topic subscription issue

--- FishPott/App/FishPottApp.swift
+++ FishPott/App/FishPottApp.swift
@@ -113,15 +113,154 @@ func getUserFirstOpenView(_ index: String) -> String {
 }
 
 
-/*
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Paystack.setDefaultPublicKey("pk_live_d93022487706c680ab03c7a8d3a285898736527c")
+//@UIApplicationMain
+class AppDelegate: NSObject, UIApplicationDelegate {
+    let gcmMessageIDKey = "gcm.message_id"
+    var notificationManager = NotificationManager() //here's where notificationManager is stored
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        Messaging.messaging().delegate = self
+
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
         return true
     }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+      if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+      }
+
+        print("userInfo 1")
+        print(userInfo)
+        guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+            //completionHandler(.failed)
+            return
+        }
+        print("new notification received")
+        handleNotification(aps: aps)
+
+      completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
 }
-*/
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+
+      let deviceToken:[String: String] = ["token": fcmToken ?? ""]
+        print("Device token: ", deviceToken) // This token can be used for testing notifications on FCM
+        setDeviceToken(token: deviceToken["token"] ?? "")
+        Messaging.messaging().subscribe(toTopic: "FISHPOT_TIPS") { error in
+          print("Subscribed to FISHPOT_TIPS topic 1")
+        }
+        Messaging.messaging().subscribe(toTopic: "FISHPOT_IOS") { error in
+          print("Subscribed to FISHPOT_IOS topic 1")
+        }
+    }
+}
+
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+
+  // Receive displayed notifications for iOS 10 devices.
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    let userInfo = notification.request.content.userInfo
+
+    if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+    }
+
+      print("userInfo 2")
+      print(userInfo)
+      guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+          //completionHandler(.failed)
+          return
+      }
+      print("new notification received")
+      handleNotification(aps: aps)
+
+    // Change this to your preferred presentation option
+    completionHandler([[.banner, .badge, .sound]])
+  }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+
+    }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    let userInfo = response.notification.request.content.userInfo
+
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID from userNotificationCenter didReceive: \(messageID)")
+    }
+
+    print(userInfo)
+      guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+          //completionHandler(.failed)
+          return
+      }
+      print("new notification received")
+      handleNotification(aps: aps)
+
+    completionHandler()
+  }
+  @discardableResult func handleNotification(aps: [String:Any]) -> Bool {
+
+        guard let alert = aps["alert"] as? String else { //get the "alert" field
+            return false
+        }
+        guard let alert = aps["alert"] as? String else { //get the "alert" field
+            return false
+        }
+        print("self.notificationManager.currentNotificationText")
+        self.notificationManager.currentNotificationText = alert
+        
+        print(self.notificationManager.currentNotificationText)
+        return true
+  }
+    
+  @discardableResult func setDeviceToken(token: String) -> Bool {
+        self.notificationManager.deviceToken = token
+        
+        saveTextInStorage("devicetoken", self.notificationManager.deviceToken ?? "")
+        print("SNMDT self.notificationManager.deviceToken: \(self.notificationManager.deviceToken)")
+        return true
+  }
+}
+
+
+/************************************************************************************/
+/************************************************************************************/
+/************************************************************************************/
+/************************************************************************************/
+
+
 /*
 //@UIApplicationMain
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -228,6 +367,11 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
 }
 */
 
+/************************************************************************************/
+/************************************************************************************/
+/************************************************************************************/
+/************************************************************************************/
+/*
 class AppDelegate: NSObject, UIApplicationDelegate {
     var notificationManager = NotificationManager() //here's where notificationManager is stored
     
@@ -338,3 +482,4 @@ extension AppDelegate {
         return true
     }
 }
+ */
